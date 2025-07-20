83590592c28c9c797bbf51d541110c436b5a7a46
tonythomasndm <ttonythomasndm@gmail.com>
2025-07-21 03:37:26 +0530

    before documentation with swagger

--- src/main/java/com/tonythomasndm/store/controllers/CartController.java
+++ src/main/java/com/tonythomasndm/store/controllers/CartController.java
@@ -4,11 +4,8 @@ import com.tonythomasndm.store.dtos.AddItemToCartRequest;
 import com.tonythomasndm.store.dtos.CartDto;
 import com.tonythomasndm.store.dtos.CartItemDto;
 import com.tonythomasndm.store.dtos.UpdateCartItemRequest;
-import com.tonythomasndm.store.entities.Cart;
-import com.tonythomasndm.store.entities.CartItem;
-import com.tonythomasndm.store.mappers.CartMapper;
-import com.tonythomasndm.store.repositories.CartRepository;
-import com.tonythomasndm.store.repositories.ProductRepository;
+import com.tonythomasndm.store.exceptions.CartNotFoundException;
+import com.tonythomasndm.store.exceptions.ProductNotFoundException;
 import com.tonythomasndm.store.services.CartService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -25,150 +22,70 @@ import java.util.UUID;
 @RequestMapping("/carts")// with restful apis we often use plural names
 public class CartController {
 
-    private final CartRepository cartRepository;
-    private final CartMapper cartMapper;
-    private final ProductRepository productRepository;
     private final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<CartDto> createCart(
-            UriComponentsBuilder uriBuilder
-    ) {
+    public ResponseEntity<CartDto> createCart(UriComponentsBuilder uriBuilder) {
         var cartDto = cartService.createCart();
-
         var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId()).toUri();
         return ResponseEntity.created(uri).body(cartDto);
-//        // why not builder one? bcoz it expects a uri - useful for public apis
-//        return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
     }
 
     @PostMapping("/{cartId}/items")
-    public ResponseEntity<?> addToCarts(
+    public ResponseEntity<CartItemDto> addToCarts(
             @PathVariable UUID cartId,
-            @Valid @RequestBody AddItemToCartRequest request) {
-
-        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.notFound().build();
-        }
-        // this approach throws an excpetion
-//        var cart = cartRepository.findById(cartId)
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        var product = productRepository.findById(request.getProductId()).orElse(null);
-        if (product == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                    Map.of("error", "product does not exist in the system")
-            );
-            //return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();// USER MADE MISTAKE HERE
-        }
-        // entities w/o function(behavior) or responsibility - anemic domain
-        // oop - combine rleated object and behavior
-        var cartItem = cart.addItemToCart(product);// more object oriented domain model
-//
-//        var cartItem = cart.getItemByProductId(product.getId());
-//
-//        if(cartItem != null) {
-//            cartItem.setQuantity(cartItem.getQuantity() + 1);
-//        } else{
-//            cartItem = new CartItem();
-//            cartItem.setProduct(product);
-//            cartItem.setQuantity(1);
-//            cartItem.setCart(cart);
-//            cart.getItems().add(cartItem);
-//            // we have two options to save it - one is to craete a differnet cartitemrepostyory or else save it in existing cartrepo
-//            // we choose second method bcoz cartitem dont have a indpendnet lifecycle - persistent throigh cart design
-//            // aggregrate root -> cart
-//            // ddd - domain driven design
-//        }
-
-        cartRepository.save(cart);
-        var cartItemDto = cartMapper.toDto(cartItem);
+            @Valid @RequestBody AddItemToCartRequest request
+    ) {
+        var cartItemDto = cartService.addToCart(cartId, request.getProductId());
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
     }
 
     @GetMapping("/{cartId}")
-    public ResponseEntity<CartDto> getCart(
+    public CartDto getCart(
             @PathVariable UUID cartId
     ) {
-        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(cartMapper.toDto(cart));
+        return cartService.getCart(cartId);
     }
 
     @PutMapping("/{cartId}/items/{productId}")
-    public ResponseEntity<?> updateItem(
+    public CartItemDto updateItem(
             @PathVariable UUID cartId,
             @PathVariable Long productId,
             @Valid @RequestBody UpdateCartItemRequest request
     ){
-        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    Map.of("error", "Cart not found.")
-            );
-            // this doenst have a bpody to inform
-            //return ResponseEntity.notFound().build();
-        }
-        var cartItem  = cart.getItemByProductId(productId);
-        if (cartItem == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    Map.of("error", "Product was not found in the cart.")
-            );
-        }//we dont have to search the product repository and check it with cartId
-
-        cartItem.setQuantity(request.getQuantity());
-        cartRepository.save(cart);
-
-        return ResponseEntity.ok(cartMapper.toDto(cartItem));
+        return cartService.updateCartItem(cartId, productId, request.getQuantity());
     }
 
     @DeleteMapping("/{cartId}/items/{productId}")
-    public ResponseEntity<?> deleteProductFromCart(
+    public ResponseEntity<Void> deleteProductFromCart(
             @PathVariable(name="cartId") UUID cartId,
             @PathVariable(name = "productId") Long productId
     ){
-        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    Map.of("error", "Cart was not found.")
-            );
-        }
-//        var cartItem  = cart.getItemByProductId(productId);
-//        if (cartItem != null) {
-//            cart.getItems().remove(cartItem);
-////            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-////                    Map.of("error", "Product was not found in the cart.")
-////            );
-//            // we dont need to be alwasy aggressive
-//        }
-
-        cart.removeItemFromCart(productId);
-        cartRepository.save(cart);
+        cartService.deleteCartItem(cartId, productId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{cartId}/items")
-    public ResponseEntity<?> clearCart(
+    public ResponseEntity<Void> clearCart(
             @PathVariable UUID cartId
     ){
-        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    Map.of("error", "Cart was not found.")
-            );
-        }
-        cart.clear();
-        cartRepository.save(cart);
+        cartService.clearCart(cartId);
         return ResponseEntity.noContent().build();
     }
-    // if we delete a crt, then the client ahs to sned another request to create a new cart
-    // which is unnecaary
 
 
+    // we cna handle this logic at central place using an excpetion handler - prefereed to keep it at bottomm
+    @ExceptionHandler(CartNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCartNotFound(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                Map.of("error", "Cart was not found.")
+        );
+    }
 
-
+    @ExceptionHandler({ProductNotFoundException.class})
+    public ResponseEntity<Map<String,String>> handleProductNotFound(){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                Map.of("error", "Product was not found in the cart.")
+        );
+    }
 }
-// in restful apis ist good to have this hearcrhy inouir resources
--- src/main/java/com/tonythomasndm/store/services/CartService.java
+++ src/main/java/com/tonythomasndm/store/services/CartService.java
@@ -9,12 +9,8 @@ import com.tonythomasndm.store.mappers.CartMapper;
 import com.tonythomasndm.store.repositories.CartRepository;
 import com.tonythomasndm.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-import org.antlr.v4.runtime.misc.LogManager;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -30,7 +26,7 @@ public class CartService {
         return cartMapper.toDto(cart);
     }
 
-    public CartItemDto getCart( UUID cartId, Long productId ){
+    public CartItemDto addToCart( UUID cartId, Long productId ){
         var cart = cartRepository.getCartWithItems(cartId).orElse(null);
         if (cart == null) {
             throw new CartNotFoundException();
@@ -38,29 +34,52 @@ public class CartService {
         var product = productRepository.findById(productId).orElse(null);
         if (product == null) {
             throw new ProductNotFoundException();
-
         }
-        // entities w/o function(behavior) or responsibility - anemic domain
-        // oop - combine rleated object and behavior
-        var cartItem = cart.addItemToCart(product);// more object oriented domain model
-//
-//        var cartItem = cart.getItemByProductId(product.getId());
-//
-//        if(cartItem != null) {
-//            cartItem.setQuantity(cartItem.getQuantity() + 1);
-//        } else{
-//            cartItem = new CartItem();
-//            cartItem.setProduct(product);
-//            cartItem.setQuantity(1);
-//            cartItem.setCart(cart);
-//            cart.getItems().add(cartItem);
-//            // we have two options to save it - one is to craete a differnet cartitemrepostyory or else save it in existing cartrepo
-//            // we choose second method bcoz cartitem dont have a indpendnet lifecycle - persistent throigh cart design
-//            // aggregrate root -> cart
-//            // ddd - domain driven design
-//        }
-
+        var cartItem = cart.addItemToCart(product);
         cartRepository.save(cart);
-        var cartItemDto = cartMapper.toDto(cartItem);
+        return cartMapper.toDto(cartItem);
     }
+
+    public CartDto getCart( UUID cartId ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null) {
+            throw new CartNotFoundException();
+        }
+        return cartMapper.toDto(cart);
+    }
+
+    public CartItemDto updateCartItem( UUID cartId, Long productId, Integer quantity ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null) {
+           throw new CartNotFoundException();
+        }
+        var cartItem  = cart.getItemByProductId(productId);
+        if (cartItem == null) {
+            throw new ProductNotFoundException();
+        }
+        cartItem.setQuantity(quantity);
+        cartRepository.save(cart);
+        return cartMapper.toDto(cartItem);
+    }
+
+    public void deleteCartItem( UUID cartId, Long productId ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null) {
+            throw new CartNotFoundException();
+        }
+        cart.removeItemFromCart(productId);
+        cartRepository.save(cart);
+    }
+
+    public void clearCart( UUID cartId ){
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null) {
+            throw new CartNotFoundException();
+        }
+        cart.clear();
+        cartRepository.save(cart);
+    }
+
 }
+
+
