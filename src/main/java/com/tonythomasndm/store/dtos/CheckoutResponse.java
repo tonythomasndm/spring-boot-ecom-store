a87c69ee37c27138ab20c51c0224a349e26b77e4
tonythomasndm <ttonythomasndm@gmail.com>
2025-07-23 05:04:55 +0530

    creating stripe session code

--- src/main/java/com/tonythomasndm/store/dtos/CheckoutResponse.java
+++ src/main/java/com/tonythomasndm/store/dtos/CheckoutResponse.java
@@ -7,4 +7,5 @@ import lombok.Data;
 @Data
 public class CheckoutResponse {
     private Long orderId;
+    private String checkoutUrl;
 }
--- src/main/java/com/tonythomasndm/store/services/CheckoutService.java
+++ src/main/java/com/tonythomasndm/store/services/CheckoutService.java
@@ -1,19 +1,21 @@
 package com.tonythomasndm.store.services;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import com.tonythomasndm.store.dtos.CheckoutRequest;
 import com.tonythomasndm.store.dtos.CheckoutResponse;
-import com.tonythomasndm.store.dtos.ErrorDto;
 import com.tonythomasndm.store.entities.Order;
 import com.tonythomasndm.store.exceptions.CartEmptyException;
 import com.tonythomasndm.store.exceptions.CartNotFoundException;
 import com.tonythomasndm.store.repositories.CartRepository;
 import com.tonythomasndm.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
-import org.antlr.v4.runtime.misc.LogManager;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+@RequiredArgsConstructor// only final variables will be intialized
 @Service
 public class CheckoutService {
     private final CartRepository cartRepository;
@@ -21,7 +23,10 @@ public class CheckoutService {
     private final AuthService authService;
     private final CartService cartService;
 
-    public CheckoutResponse checkout(CheckoutRequest request) {
+    @Value("${websiteUrl}")
+    private String websiteUrl;
+
+    public CheckoutResponse checkout(CheckoutRequest request) throws StripeException {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if(cart == null){
             throw new CartNotFoundException();
@@ -32,8 +37,34 @@ public class CheckoutService {
         }
         var order = Order.fromCart(cart, authService.getCurrentUser());
         orderRepository.save(order);
+
+        // create a checkout session
+        var builder = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl(websiteUrl + "/checkout-success?order_id=" + order.getId())
+                .setCancelUrl(websiteUrl + "/checkout-cancel");
+
+        order.getItems().forEach(item -> {
+            var lineItem = SessionCreateParams.LineItem.builder()
+                    .setQuantity(Long.valueOf(item.getQuantity()))
+                    .setPriceData(
+                            SessionCreateParams.LineItem.PriceData.builder()
+                                    .setCurrency("usd")
+                                    .setUnitAmountDecimal(item.getUnitPrice())
+                                    .setProductData(
+                                            SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                    .setName(item.getProduct().getName())
+                                                    .build()
+                                    ).build()
+                    ).build();
+            builder.addLineItem(lineItem);
+        });
+
+        var session = Session.create(builder.build());
+
+
         cartService.clearCart(cart.getId());
-        return new CheckoutResponse(order.getId());
+        return new CheckoutResponse(order.getId(), session.getUrl());
 
     }
 }
